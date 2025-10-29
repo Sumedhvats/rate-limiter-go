6b4f5eb029d8f83e4483ce3cd0501d11089153a5
Sumedh Vats <sumedhvats2004@gmail.com>
2025-10-30 01:40:18 +0530

    added in memory storage

--- pkg/storage/memory.go
+++ pkg/storage/memory.go
@@ -1,1 +1,97 @@
 package storage
+
+import (
+	"errors"
+
+	"sync"
+	"time"
+)
+type MemoryStorage struct{
+	data sync.Map
+	cleanup *time.Ticker
+}
+type memoryEntry struct{
+	value interface{}
+	expiresAt time.Time
+}
+func NewMemoryStorage() *MemoryStorage{
+	s:=&MemoryStorage{
+		cleanup: time.NewTicker(1*time.Minute),
+	}
+	go s.cleanupExpired()
+	return s
+}
+func (s *MemoryStorage) cleanupExpired(){
+	for range s.cleanup.C{
+		now :=time.Now()
+		s.data.Range(func(key, value interface{}) bool {
+			entry:=value.(memoryEntry)
+			if now.After(entry.expiresAt){
+				s.data.Delete(key)
+			}
+			return true
+		})
+		
+	}
+}
+func (s *MemoryStorage) Get(key string)(interface{},error){
+	val,ok := s.data.Load(key)
+	if !ok{
+		return nil,nil
+	}
+	entry:=val.(*memoryEntry)
+   if time.Now().After(entry.expiresAt) {
+        s.data.Delete(key)
+        return nil, nil
+    }
+	return entry.value, nil
+}
+func (s *MemoryStorage) Set(key string,value interface{}, ttl time.Duration)error{
+	now:= time.Now()
+	entry:=&memoryEntry{
+		value: value,
+		expiresAt: now.Add(ttl),
+	}
+   s.data.Store(key,entry)
+   return nil
+}
+
+func (s *MemoryStorage) Delete(key string){
+s.data.Delete(key)
+}
+
+func (s *MemoryStorage) Increment(key string, amount int) (int64, error) {
+	for {
+		entryAny, ok := s.data.Load(key)
+		if !ok {
+			entry := &memoryEntry{
+				value:     int64(amount),
+				expiresAt: time.Now().Add(10 * time.Minute),
+			}
+			s.data.Store(key, entry)
+			return int64(amount), nil
+		}
+
+		entry, ok := entryAny.(*memoryEntry)
+		if !ok {
+			return 0, errors.New("invalid entry type")
+		}
+
+		currentValue, ok := entry.value.(int64)
+		if !ok {
+			return 0, errors.New("value is not int64")
+		}
+
+		newValue := currentValue + int64(amount)
+		newEntry := &memoryEntry{
+			value:     newValue,
+			expiresAt: entry.expiresAt,
+		}
+
+		swapped := s.data.CompareAndSwap(key, entry, newEntry)
+		if swapped {
+			return newValue, nil
+		}
+		// else retry
+	}
+}
---
+++ pkg/storage/memory_test.go
@@ -0,0 +1,72 @@
+package storage
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemoryStorate_GetSet(t *testing.T) {
+	store:= NewMemoryStorage()
+
+	err:=store.Set("123.234.15.1","value1",1*time.Minute)
+	assert.NoError(t,err)
+
+	val,err:=store.Get("123.234.15.1")
+	assert.NoError(t,err)
+	assert.Equal(t,"value1",val)
+}
+
+func TestExpiration (t *testing.T){
+	  store := NewMemoryStorage()
+
+    // Set key with short TTL
+    ttl := 100* time.Millisecond
+	err:=store.Set("tempKey","tempValue",ttl)
+	assert.NoError(t,err)
+
+	val, err := store.Get("tempKey")
+    assert.NoError(t, err)
+    assert.Equal(t, "tempValue", val)
+
+	time.Sleep(ttl+100*time.Millisecond)
+	val, err = store.Get("tempKey")
+    assert.NoError(t, err)
+    assert.Nil(t, val)
+}
+
+func TestMemoryStorate_Increment(t *testing.T){
+	store:=NewMemoryStorage()
+	//Increment non-existent key by 5 should return 5
+	val,err:=store.Increment("counter",5)
+	  assert.NoError(t, err)
+    assert.Equal(t, int64(5), val)
+
+	//Increment again by 3 should return 8
+	val,err=store.Increment("counter",3)
+	  assert.NoError(t, err)
+    assert.Equal(t, int64(8), val)
+	
+	got,err:=store.Get("counter")
+	  assert.NoError(t, err)
+    assert.Equal(t, int64(8), got.(int64))
+}
+
+func TestConcurrentAccess(t *testing.T){
+	store:=NewMemoryStorage()
+	var wg sync.WaitGroup
+	for i:=0;i<100;i++{
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			_,err:=store.Increment("temp",100)
+			assert.NoError(t,err)
+		}()
+		}
+		wg.Wait()
+			currVal,err:=store.Get("temp")
+		assert.NoError(t,err)
+		assert.Equal(t,currVal,int64(10000))
+}
