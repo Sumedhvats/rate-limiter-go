d0338795f653e38eaeddcff461f805de506b8148
Sumedh Vats <sumedhvats2004@gmail.com>
2025-10-29 11:22:11 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sumedhvats/rate-limiter-go
+
+go 1.25.1
---
+++ pkg/limiter/limiter.go
@@ -0,0 +1,26 @@
+package limiter
+
+import "time"
+type stats struct{
+	Limit int
+	Remaining int
+	ResetAt time.Time
+}
+type Limiter interface{
+	Allow(key string)(bool,error)
+	AllowN(key string ,n int)(bool,error)
+	Reset(key string) error
+	GetStats(key string)(*stats,error)
+}
+type config struct{
+	Rate int
+	Window time.Time
+	Burst int
+}
+
+type Storage interface{
+	Get(key string)(interface{},error)
+	Set(key string , value interface{}, ttl time.Duration)error
+	Increment(key string, value int)(int64,error)
+	Delete(key string)error
+}
